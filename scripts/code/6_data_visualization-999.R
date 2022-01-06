fd52cb06aee8b140eba0b1fdc382f02f9d784e10
Lang <k.m.lang@uu.nl>
2022-01-06 22:50:40 +0100

    outlining the data viz script

--- scripts/code/6_data_visualization-999.R
+++ scripts/code/6_data_visualization-999.R
@@ -1,4 +1,53 @@
 ### Title:    Introduction to R 5: Data Visualization
 ### Author:   Kyle M. Lang
 ### Created:  2022-01-04
-### Modified: 2022-01-04
+### Modified: 2022-01-06
+
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+library(lattice)
+
+###-Base R Graphics----------------------------------------------------------###
+
+## plot()
+
+## hist()
+
+## boxplot()
+
+## scatterplot matrices
+
+## kernel density plots
+
+## lines()
+
+## par()
+
+
+###-GGPlot-------------------------------------------------------------------###
+
+## Building up a plot
+
+## Different flavors of geom
+
+## Applying styling in different places
+
+## Different themes
+
+## Modifying theme elements
+
+## Facetting
+
+## Pasting together seperate plots
+
+## Modifying grobs
+
+
+###-Lattice------------------------------------------------------------------###
+
+## Conditional plots
+
+
+###-Saving Graphics----------------------------------------------------------###
+
